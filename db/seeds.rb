a6cb2b20f5f6d21f8c38810cd49b239bcad55e68
Leon Boutrand <leonboutrand@gmail.com>
2019-02-13 16:02:09 +0100

    ex3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Good house',
+  address: '10 Cours Eile zola fsdfdsf',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. sdfsdfksd fskdfjsndf ksjdfns dkfs df! Quibusdam iste, quas voluptates.',
+  price_per_night: 4,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Shitty house sfgj foieru tdmfng eor dfng ert',
+  address: '45 hjksuiobvcuire',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. sdfsdfksd fskdfjsndf ksjdfns dkfs df facere maiores, optio illo.',
+  price_per_night: 100,
+  number_of_guests: 10
+)
+
+Flat.create!(
+  name: 'Amazing house',
+  address: '487 tr uyt re gf ytr gf r tr gf hggfdgfgfdgf',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. sdfsdfksd fskdfjsndf ksjdfns dkfs df! Quaerat, eligendi neque ea!',
+  price_per_night: 75,
+  number_of_guests: 3
+)
